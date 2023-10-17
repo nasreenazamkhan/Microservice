5c7f9c3ceab35abf29b34bc178453e8694520b52
Nasreen <nasrKhan@ega.ae>
2023-10-17 09:31:05 +0400

    'First'

---
+++ api-gateway/src/test/java/com/nasreen/demo/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nasreen.demo.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
